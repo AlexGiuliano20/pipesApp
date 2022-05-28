22574c36e87191c7cb2c720500cb9c21a4cd096f
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-05-28 16:04:31 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'alex giuliano';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Alex'
+  }
+
+  mostrarNombre(): void {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
