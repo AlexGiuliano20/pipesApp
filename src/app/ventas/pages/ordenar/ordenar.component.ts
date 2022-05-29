6d08b489298570cba01875e99342dff8ecb7b1db
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-05-29 00:02:35 -0300

    Tabla llenada

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
   styles: [],
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
   enMayusculas: boolean = true;
-
-  constructor() {}
-
-  ngOnInit(): void {}
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Hulk',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
 
   cambiar() {
     this.enMayusculas = !this.enMayusculas;
