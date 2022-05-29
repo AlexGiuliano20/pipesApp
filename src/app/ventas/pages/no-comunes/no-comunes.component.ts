63255c5729da0c4aa81426fb7aff447ec418d40f
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-05-28 23:13:22 -0300

    Async Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -62,4 +63,13 @@ export class NoComunesComponent {
       vuela: false,
     },
   ];
+
+  // Async Pipe
+  miObservable = interval(2000);
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('La promesa ha sido resuelta!');
+    }, 3500);
+  });
 }
