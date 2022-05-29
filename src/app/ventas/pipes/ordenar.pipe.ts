deab370389b00bbf0c6f59310920428e82dbe890
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-05-29 00:30:09 -0300

    Pipe ordenar ordena tabla por cada uno de sus atributos

--- src/app/ventas/pipes/ordenar.pipe.ts
+++ src/app/ventas/pipes/ordenar.pipe.ts
@@ -5,8 +5,16 @@ import { Heroe } from '../interfaces/ventas.interfaces';
   name: 'ordenar',
 })
 export class OrdenarPipe implements PipeTransform {
-  transform(value: Heroe[]): Heroe[] {
-    value = value.sort((a, b) => (a.nombre > b.nombre ? 1 : -1));
-    return value;
+  transform(value: Heroe[], ordenarPor: string = 'sin valor'): Heroe[] {
+    switch (ordenarPor) {
+      case 'nombre':
+        return (value = value.sort((a, b) => (a.nombre > b.nombre ? 1 : -1)));
+      case 'vuela':
+        return (value = value.sort((a, b) => (a.vuela > b.vuela ? -1 : 1)));
+      case 'color':
+        return (value = value.sort((a, b) => (a.color > b.color ? 1 : -1)));
+      default:
+        return value;
+    }
   }
 }
