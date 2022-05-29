e04ffc022ddb3797e3ea267dee38eabb7cee035b
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-05-28 22:08:44 -0300

    agregado a los i18 pipes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './no-comunes.component.html',
   styles: [],
 })
-export class NoComunesComponent {}
+export class NoComunesComponent {
+  // i18nSelect
+  nombre: string = 'Susana';
+  genero: string = 'femenino';
+
+  invitacionMap = {
+    masculino: 'invitarlo',
+    femenino: 'invitarla',
+  };
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Ramón', 'Ricardo', 'San Martin'];
+  clientesMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    other: 'tenemos # clientes esperando.',
+  };
+
+  cambiarCliente(): void {
+    this.nombre == 'Susana'
+      ? ((this.nombre = 'Alex'), (this.genero = 'masculino'))
+      : ((this.nombre = 'Susana'), (this.genero = 'femenino'));
+  }
+
+  borrarCliente(): void {
+    this.clientes.pop();
+  }
+}
